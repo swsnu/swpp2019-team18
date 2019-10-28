febe3cf57660556573aee716263c620851cf440d
Namkoong Hugh <mmsori@snu.ac.kr>
2019-10-28 17:13:46 +0900

    Modify backend directory structure

--- backend/diary/urls.py
+++ backend/diary/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from diary import views
+from .views import auth
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin, name = 'signin'),
-    path('signout/', views.signout, name='signout'),
-    path('token/', views.token)
+    path('signup/', auth.signup, name='signup'),
+    path('signin/', auth.signin, name = 'signin'),
+    path('signout/', auth.signout, name='signout'),
+    path('token/', auth.token)
 ]
