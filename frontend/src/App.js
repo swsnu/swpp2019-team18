1eea34713132750b328a8711047bd1b9120d0c15
lim8540 <lim8540@snu.ac.kr>
2019-11-04 19:17:07 +0900

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 import './App.css';
 import Login from './container/authentication/Login'
 import SignUp from './container/authentication/SignUp'
@@ -7,43 +8,40 @@ import EditDiary from './container/diaryEdit/editDiary';
 import MyDiaryList from './container/MyDiaryList/MyDiaryList';
 import Sidebar from './container/sidebar/sidebar'
 
+
 import {  Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router';
 
-
-class App extends Component {
+/*class App extends Component {
   componentDidMount(){
     axios.get('/api/token/');
-  }
+  }*/
 
-  render() {
-    return (
-      <ConnectedRouter history={props.history}>
-        <div>
-          <Switch>
-  
-            <Route path='/login' exact component={Login}/>
-            <Route path='/signup' exact component={SignUp}/>
-            
-  
-            <div>
-              <div className = 'sidebar'>
-                <Sidebar/>
-              </div>
-              <div className = 'main'>
-              <Route path='/diary' exact component={MyDiaryList}/>
-              <Route path='/diary/create' exact component={NewDiary}/>
-              <Route path='/diary/:id/edit' exact component={EditDiary}/>
-              </div>
+function App(props) {
+  return (
+    <ConnectedRouter history={props.history}>
+      <div>
+        <Switch>
+
+          <Route path='/login' exact component={Login}/>
+          <Route path='/signup' exact component={SignUp}/>
+          
+
+          <div>
+            <div className = 'sidebar'>
+              <Sidebar/>
             </div>
+            <div className = 'main'>
+            <Route path='/diary' exact component={MyDiaryList}/>
+            <Route path='/diary/create' exact component={NewDiary}/>
+            <Route path='/diary/:id/edit' exact component={EditDiary}/>
+            </div>
+          </div>
           </Switch>
-        </div>    
-      </ConnectedRouter>
-    )
-  }
-
+      </div>    
+    </ConnectedRouter>
+  )
 }
-  
-
 
 export default App;
+
